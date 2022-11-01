c7969f2fccd7cf0de2f3b696e4570c91e577a173
jasonxuyang <jasonxuyang@gmail.com>
2022-11-01 18:21:19 -0400

    add console log in bot.js

--- bot.js
+++ bot.js
@@ -1,37 +1,40 @@
 // Our Twitter library
-var Twit = require('twit');
+var Twit = require("twit");
+console.log("hello world");
 
 // We need to include our configuration file
-var T = new Twit(require('./config.js'));
+var T = new Twit(require("./config.js"));
 
 // This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
+var mediaArtsSearch = { q: "#mediaarts", count: 10, result_type: "recent" };
 
 // This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
 function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
-	  // log out any errors and responses
-	  console.log(error, data);
-	  // If our search request to the server had no errors...
-	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
-		var retweetId = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
-	  }
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-	});
+  T.get("search/tweets", mediaArtsSearch, function (error, data) {
+    // log out any errors and responses
+    console.log(error, data);
+    // If our search request to the server had no errors...
+    if (!error) {
+      // ...then we grab the ID of the tweet we want to retweet...
+      var retweetId = data.statuses[0].id_str;
+      // ...and then we tell Twitter we want to retweet it!
+      T.post("statuses/retweet/" + retweetId, {}, function (error, response) {
+        if (response) {
+          console.log(
+            "Success! Check your bot, it should have retweeted something."
+          );
+        }
+        // If there was an error with our Twitter call, we print it out here.
+        if (error) {
+          console.log("There was an error with Twitter:", error);
+        }
+      });
+    }
+    // However, if our original search request had an error, we want to print it out here.
+    else {
+      console.log("There was an error with your hashtag search:", error);
+    }
+  });
 }
 
 // Try to retweet something as soon as we run the program...
