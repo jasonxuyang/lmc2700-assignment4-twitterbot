f00bc12437a8759dd158b30b2113b3ffaf657a2c
sarahnicoara <snicoara3@gatech.edu>
2022-11-01 18:38:18 -0400

    Our Twitter keys

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key:         "XxLUZWHRNLcmnsPtVVILCXkjO",
+  consumer_secret:      "yVKxcXl8AtfYZMgJ39EB21pVunzlc7yX80049hO7mBSxsO3zuf",
+  access_token:         "1585365243747659776-5zTzzujSa3oNK7Ujq9MLJmR6dnNJJU",
+  access_token_secret:  "4mFb8gIzMRQnRy3aepeAOA6wBbyw1iLzkup3kehFjGGMm",
 }
